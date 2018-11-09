ae7b6e63f7040c3502a7867e692274679233c916
James Stuckey <jstuckey@rutgers.edu>
2018-11-09 14:47:04 -0500

    required env in the config file

--- index.js
+++ index.js
@@ -11,31 +11,38 @@ const { dbConnect } = require('./db-mongoose');
 const app = express();
 
 app.use(
-  morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
-    skip: (req, res) => process.env.NODE_ENV === 'test'
-  })
+    morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
+        skip: (req, res) => process.env.NODE_ENV === 'test'
+    })
 );
 
 app.use(
-  cors({
-    origin: CLIENT_ORIGIN
-  })
+    cors({
+        origin: CLIENT_ORIGIN
+    })
 );
 
+// parses the request body
+app.use(express.json());
+
+app.use('/api/blog', (req, res, next) => {
+    console.log('Working!');
+});
+
 function runServer(port = PORT) {
-  const server = app
-    .listen(port, () => {
-      console.info(`App listening on port ${server.address().port}`);
-    })
-    .on('error', err => {
-      console.error('Express failed to start');
-      console.error(err);
-    });
+    const server = app
+        .listen(port, () => {
+            console.info(`App listening on port ${server.address().port}`);
+        })
+        .on('error', err => {
+            console.error('Express failed to start');
+            console.error(err);
+        });
 }
 
 if (require.main === module) {
-  dbConnect();
-  runServer();
+    dbConnect();
+    runServer();
 }
 
 module.exports = { app };

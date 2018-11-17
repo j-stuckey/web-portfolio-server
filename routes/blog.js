31661362222f2d449433618c6b78a283cf3aec5b
James Stuckey <jstuckey@rutgers.edu>
2018-11-16 23:12:16 -0500

    added route for GET blog

--- routes/blog.js
+++ routes/blog.js
@@ -4,11 +4,17 @@ const express = require('express');
 const mongoose = require('mongoose');
 const passport = require('passport');
 
+const Blog = require('../models/blog');
+
 // starts an instance of router for the route
 const router = express.Router();
 
-router.get('/api/blog', (req, res, next) => {
-    console.log('Working!');
+router.get('/', (req, res, next) => {
+    Blog.find()
+        .then(results => {
+            res.json(results);
+        })
+        .catch(err => next(err));
 });
 
 module.exports = router;
